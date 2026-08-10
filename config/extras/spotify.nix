87248ea2926b63137287c7447c8f614cec365d18
Jakub Skowron <jakubskowron766@proton.me>
2026-08-10 22:30:36 +0200

    feat(nixos/extras/spotify): add "startup-page" extension to spicetify

--- config/extras/spotify.nix
+++ config/extras/spotify.nix
@@ -12,6 +12,15 @@ in
 			adblock
 			fullAppDisplay
 			volumePercentage
+			{
+				src = "${pkgs.fetchFromGitHub {
+					owner = "Resxt";
+					repo = "Spicetify-Extensions";
+					rev = "fb94b32511b74f791ddeb025aec0c77928d6bd60";
+					hash = "sha256-SLu2+H5tdwPz0JrT61SuAx9uSW7Wfv2wLoA7d/AwmZQ=";
+				}}/startup-page/dist";
+				name = "startup-page.js";
+			}
 		];
 	};
 
