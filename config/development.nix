639ae66af72f593972ca19eae891e9a59ee9c04a
Jakub Skowron <jakubskowron766@proton.me>
2026-09-06 22:55:43 +0200

    feat(nixos/development): remove npm and claude-code

--- config/development.nix
+++ config/development.nix
@@ -1,15 +1,12 @@
 { pkgs, ... }:
 
 {
-	programs.npm.enable = true;
-
 	environment.systemPackages = with pkgs; [
 		# cmake
 		# gdb
 		gitleaks
 		osv-scanner
 		# zig
-		claude-code
 		python3
 		semgrep
 	];
