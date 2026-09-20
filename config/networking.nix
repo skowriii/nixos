8d425695a248f0ef479fbc61fc02f0e77160ce2f
Jakub Skowron <jakubskowron766@proton.me>
2026-09-21 00:23:09 +0200

    refactor(networking): change indents

--- config/networking.nix
+++ config/networking.nix
@@ -7,14 +7,14 @@
 			enable = true;
 			allowedTCPPorts = [
 				31536 # qBittorrent - incoming connections from peers
-					57621 # Spotify Connect
+				57621 # Spotify Connect
 			];
 			allowedUDPPorts = [
 				1900 # SSDP - qBittorrent and Spotify
-					5353 # mDNS - Spotify Connect
-					6771 # qBittorrent
-					31536 # qBittorrent
-					57621 # Spotify Connect
+				5353 # mDNS - Spotify Connect
+				6771 # qBittorrent
+				31536 # qBittorrent
+				57621 # Spotify Connect
 			];
 		};
 		networkmanager = {
