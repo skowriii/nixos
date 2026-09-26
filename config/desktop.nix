74c96715d058aad08d3aa9523ffeb41ce3abaab8
Jakub Skowron <jakubskowron766@proton.me>
2026-09-26 13:36:30 +0200

    refactor(desktop): remove old zen-browser buildFHSEnv override

--- config/desktop.nix
+++ config/desktop.nix
@@ -23,18 +23,6 @@
 
 			fuzzel
 
-			# (pkgs.buildFHSEnv {
-			# 	name = "zen";
-			# 	targetPkgs = pkgs: (with pkgs; [
-			# 		ffmpeg-full
-			# 		libva
-			# 		mesa
-			# 		libglvnd
-			# 		alsa-lib
-			# 		pipewire
-			# 	]);
-			# 	runScript = "${inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/zen";
-			# })
 			inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
 
 			# File browser related
